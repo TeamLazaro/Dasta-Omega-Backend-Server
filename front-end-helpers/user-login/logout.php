363f203af6ef91d5c9891c7d078dba6808812dec
adi <adityabhat@lazaro.in>
2018-05-04 20:20:14 +0530

    The Logout Process; Added an HTML redirect as opposed to an HTTP one

--- front-end-helpers/user-login/logout.php
+++ front-end-helpers/user-login/logout.php
@@ -4,8 +4,30 @@
 setcookie( 'auth', '', time() - 999, '/' );
 
 // Re-direct back to the front-end, else leave a message
-if ( ! empty( $_GET[ 'redirect' ] ) ) {
-	header( 'Location: ' . $_GET[ 'redirect' ] );
-} else {
-	echo "You've been logged out.";
-}
+// if ( ! empty( $_GET[ 'redirect' ] ) ) {
+// 	header( 'Location: ' . $_GET[ 'redirect' ] );
+// } else {
+// 	echo "You've been logged out.";
+// }
+$redirectTo = $_GET[ 'redirect' ] ?? 'http://dasta.in/';
+
+?>
+<!DOCTYPE html>
+<html>
+
+<head>
+  	<meta http-equiv="refresh" content="0; URL=<?php echo $redirectTo; ?>" />
+	<title>Logging out of Dasta.....</title>
+</head>
+
+<body>
+
+	<script type="text/javascript">
+
+		window.location = "<?php echo $redirectTo; ?>";
+
+	</script>
+
+</body>
+
+</html>
