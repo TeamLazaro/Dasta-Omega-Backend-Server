d30a2c909a3fa14b344d6fc0f0a5c39e3c4b548f
adi <adityabhat@lazaro.in>
2018-05-29 11:49:14 +0530

    Generalize the "create-lead" and "get-user-by-phone" helpers to work in CLI too

---
+++ end-points/create-lead/index.php
@@ -0,0 +1,134 @@
+<?php
+
+/*
+ *
+ * -/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/
+ * SCRIPT SETUP
+ * /-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-
+ *
+ */
+ini_set( 'display_errors', 0 );
+ini_set( 'error_reporting', E_ALL );
+
+// Determine execution environment
+if ( http_response_code() === false ) {
+	$scriptInterface = 'command-line';
+	$isCommandLineInterface = true;
+	$isWebInterface = false;
+} else {
+	$scriptInterface = 'web';
+	$isCommandLineInterface = false;
+	$isWebInterface = true;
+}
+
+// Set the timezone
+date_default_timezone_set( 'Asia/Kolkata' );
+// Do not let this script timeout
+set_time_limit( 0 );
+
+if ( $isCommandLineInterface ) {
+
+	if ( empty( $argv[ 1 ] ) ) {
+		$response[ 'message' ] = 'No input provided.';
+		fwrite( STDERR, json_encode( $response ) );
+		exit( 69 );
+	}
+	try {
+		parse_str( $argv[ 1 ], $input );
+	} catch ( Exception $e ) {
+		$response[ 'message' ] = 'Error in processing input. ' . $e->getMessage();
+		fwrite( STDERR, json_encode( $response ) );
+		exit( 69 );
+	}
+
+}
+
+if ( $isWebInterface ) {
+
+	// continue processing this script even if
+	// the user closes the tab, or
+	// hits the ESC key
+	ignore_user_abort( true );
+
+	// Allow cross-origin requests
+	header( 'Access-Control-Allow-Origin: *' );
+
+	// Respond in JSON format
+	header( 'Content-Type: application/json' );
+
+	$input = &$_REQUEST;
+
+}
+
+
+
+
+
+/*
+ *
+ * -/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/
+ * SCRIPT CORE
+ * /-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-
+ *
+ */
+require __DIR__ . '/lib/crm.php';
+
+
+
+// Check if required input is present and valid
+if ( empty( $input[ 'phoneNumber' ] ) ) {
+	$response[ 'code' ] = 0;
+	$response[ 'message' ] = 'No phone number was provided.';
+	if ( $isCommandLineInterface ) {
+		fwrite( STDERR, json_encode( $response ) );
+		exit( 69 );
+	}
+	if ( $isWebInterface ) {
+		http_response_code( 500 );
+		die( json_encode( $response ) );
+	}
+}
+
+// Pull all the input data
+$phoneNumber = $input[ 'phoneNumber' ];
+// $unit = $input[ 'unit' ];
+$firstName = $input[ 'firstName' ] ?? 'lazaro test';
+$lastName = $input[ 'lastName' ] ?? 'hia there';
+$leadStatus = $input[ 'leadStatus' ] ?? 'Fresh';
+$leadSource = $input[ 'leadSource' ] ?? 'Web Pricing';
+$leadData = [
+	'Lead Status' => $leadStatus,
+	'Lead Source' => $leadSource,
+	'First Name' => $firstName,
+	'Last Name' => $lastName,
+	'Phone' => $phoneNumber
+];
+
+try {
+
+	// Create the lead
+	$lead = CRM\createLead( $leadData );
+
+	// Construct a response and respond back
+	$response[ 'code' ] = 1;
+	$response[ 'data' ] = [
+		'id' => $lead->id ?? ''
+	];
+	$response[ 'message' ] = 'Lead created.';
+	die( json_encode( $response ) );
+
+} catch ( Exception $e ) {
+
+	// Respond with an error
+	$response[ 'code' ] = -1;
+	$response[ 'message' ] = $e->getMessage();
+	if ( $isCommandLineInterface ) {
+		fwrite( STDERR, json_encode( $response ) );
+		exit( 69 );
+	}
+	if ( $isWebInterface ) {
+		http_response_code( 500 );
+		die( json_encode( $response ) );
+	}
+
+}
---
+++ end-points/get-user-by-phone/index.php
@@ -0,0 +1,138 @@
+<?php
+
+/*
+ *
+ * -/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/
+ * SCRIPT SETUP
+ * /-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-
+ *
+ */
+ini_set( 'display_errors', 0 );
+ini_set( 'error_reporting', E_ALL );
+
+// Determine execution environment
+if ( http_response_code() === false ) {
+	$scriptInterface = 'command-line';
+	$isCommandLineInterface = true;
+	$isWebInterface = false;
+} else {
+	$scriptInterface = 'web';
+	$isCommandLineInterface = false;
+	$isWebInterface = true;
+}
+
+// Set the timezone
+date_default_timezone_set( 'Asia/Kolkata' );
+// Do not let this script timeout
+set_time_limit( 0 );
+
+if ( $isCommandLineInterface ) {
+
+	if ( empty( $argv[ 1 ] ) ) {
+		$response[ 'message' ] = 'No input provided.';
+		fwrite( STDERR, json_encode( $response ) );
+		exit( 69 );
+	}
+	try {
+		parse_str( $argv[ 1 ], $input );
+	} catch ( Exception $e ) {
+		$response[ 'message' ] = 'Error in processing input. ' . $e->getMessage();
+		fwrite( STDERR, json_encode( $response ) );
+		exit( 69 );
+	}
+
+}
+
+if ( $isWebInterface ) {
+
+	// continue processing this script even if
+	// the user closes the tab, or
+	// hits the ESC key
+	ignore_user_abort( true );
+
+	// Allow cross-origin requests
+	header( 'Access-Control-Allow-Origin: *' );
+
+	// Respond in JSON format
+	header( 'Content-Type: application/json' );
+
+	$input = &$_REQUEST;
+
+}
+
+
+
+
+
+/*
+ *
+ * -/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/
+ * SCRIPT CORE
+ * /-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-
+ *
+ */
+require __DIR__ . '/lib/crm.php';
+
+
+
+
+try {
+
+	$phoneNumber = $input[ 'phoneNumber' ];
+
+	if ( empty( $phoneNumber ) ) {
+		$response[ 'code' ] = 0;
+		throw new Exception( 'No phone number was provided.' );
+	}
+
+	// Search for a matching prospect
+	$prospect = CRM\getProspect( $phoneNumber );
+	if ( ! empty( $prospect ) ) {
+		$response[ 'code' ] = 1;
+		$response[ 'message' ] = 'Found a prospect.';
+		$response[ 'data' ] = [
+			'id' => $prospect[ 'CONTACTID' ] ?? '',
+			'name' => $prospect[ 'Full Name' ] ?? '',
+			'firstName' => $prospect[ 'First Name' ] ?? '',
+			'lastName' => $prospect[ 'Last Name' ] ?? '',
+			'email' => $prospect[ 'Email' ] ?? ''
+		];
+		die( json_encode( $response ) );
+	}
+
+	// If no prospect was found, search for a matching lead
+	$lead = CRM\getLead( $phoneNumber );
+	if ( ! empty( $lead ) ) {
+		$response[ 'code' ] = 1;
+		$response[ 'message' ] = 'Found a lead.';
+		$response[ 'data' ] = [
+			'id' => $lead[ 'LEADID' ] ?? '',
+			'name' => $lead[ 'Full Name' ] ?? '',
+			'firstName' => $lead[ 'First Name' ] ?? '',
+			'lastName' => $lead[ 'Last Name' ] ?? '',
+			'email' => $lead[ 'Email' ] ?? ''
+		];
+		die( json_encode( $response ) );
+	}
+
+
+	// If no prospect or lead was found
+	$response[ 'code' ] = 2;
+	$response[ 'message' ] = 'No matching lead or prospect was found.';
+	die( json_encode( $response ) );
+
+} catch ( Exception $e ) {
+
+	// Respond with an error
+	$response[ 'code' ] = -1;
+	$response[ 'message' ] = $e->getMessage();
+	if ( $isCommandLineInterface ) {
+		fwrite( STDERR, json_encode( $response ) );
+		exit( 69 );
+	}
+	if ( $isWebInterface ) {
+		http_response_code( 500 );
+		die( json_encode( $response ) );
+	}
+
+}
